c820a253dd87f2d5100d6b7a625174119f295569
mato8696-creator <mato8696@gmail.com>
2026-01-18 09:02:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,14 +69,3 @@ with st.sidebar:
     pw = st.text_input("Password:", type="password")
     if pw == "matin2026":
         st.metric("بینەرێن ئەڤڕۆ", st.session_state.count)
-st.markdown("""
-<style>
-    /* ئەڤ کۆدە نیشانێن زێدە ل ڕەخ ناڤی لادەت */
-    .viewerBadge_link__1S137 { display: none !important; }
-    .stApp a.header-anchor { display: none !important; }
-    
-    /* پاراستنا پاقژیا سایتی */
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-</style>
-""", unsafe_allow_html=True)
