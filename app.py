f681efc80efcb96a5afc9f8f577f589c96cb7bf6
mato8696-creator <mato8696@gmail.com>
2026-01-18 08:59:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 st.markdown("""
 <style>
-    /* ئەڤ کۆدە نیشانێن زێدە ل ڕەخ ناڤی لادەت */
-    .viewerBadge_link__1S137 { display: none !important; }
+    /* 1. لادانا وێ نیشانا بچووک ل ڕەخ ناڤی دا خەلک کلیکێ لێ نەکەن */
     .stApp a.header-anchor { display: none !important; }
     
-    /* پاراستنا پاقژیا سایتی */
+    /* 2. ڤەشارتنا مێنۆیا سەرەکی و فووتەرێ ستریملیت دا سایت فەرمیتر ببیت */
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
+    header {visibility: hidden;}
+
+    /* 3. پاراستنا ستایلێ بینەران */
+    .stMetric { background-color: rgba(255, 255, 255, 0.05); padding: 10px; border-radius: 10px; }
 </style>
 """, unsafe_allow_html=True)
